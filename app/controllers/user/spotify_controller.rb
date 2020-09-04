d65ba3204304ef2b7a6bea8ee6418ac07be5035c
vagrant <vagrant@localhost.localdomain>
2020-09-04 11:04:29 +0000

    add_spotify

--- app/controllers/user/spotify_controller.rb
+++ app/controllers/user/spotify_controller.rb
@@ -4,11 +4,15 @@ class User::SpotifyController < ApplicationController
     RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
 
     def index
+        if params[:search].blank?
+            redirect_back(fallback_location: root_path)
+        end
         @search_word = params[:search]
         # アーティスト検索
             if @search_word.present?
                 @searchartists = RSpotify::Artist.search(@search_word)
             end
+            # 一覧表示件数
             if params[:artist_count].blank?
                 @artist_count = 4
             else
@@ -18,6 +22,7 @@ class User::SpotifyController < ApplicationController
             if @search_word.present?
                 @searchalbums = RSpotify::Album.search(@search_word)
             end
+            # 一覧表示件数
             if params[:album_count].blank?
                 @album_count = 4
             else
@@ -27,10 +32,29 @@ class User::SpotifyController < ApplicationController
             if @search_word.present?
                 @searchsongs = RSpotify::Track.search(@search_word)
             end
+            # 一覧表示件数
             if params[:song_count].blank?
                 @song_count = 4
             else
                 @song_count = params[:song_count].to_i
             end
     end
+
+    def artist_show
+        @artist = RSpotify::Artist.find(params[:id])
+        @albums = @artist.albums
+    end
+
+    def album_show
+        @album = RSpotify::Album.find(params[:id])
+        @songs = @album.tracks
+    end
+
+    def song_show
+        @song = RSpotify::Track.find(params[:id])
+        @album = @song.album
+    end
+    
+    
+    
 end
