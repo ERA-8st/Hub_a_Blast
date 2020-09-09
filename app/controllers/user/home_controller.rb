6069b43aac9aa2e547d88292c76efb6430ca89b1
vagrant <vagrant@localhost.localdomain>
2020-09-09 08:40:24 +0000

    remove_same_albums

--- app/controllers/user/home_controller.rb
+++ app/controllers/user/home_controller.rb
@@ -1,7 +1,14 @@
 class User::HomeController < ApplicationController
 
+	require 'rspotify'
+	RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
+
 	def top
-		
+		if current_user.present?
+			@song_comments = current_user.song_comments.limit(3)
+		end
+		@new_releases =  RSpotify::Album.new_releases
+		@new_releases_in_jp =  RSpotify::Album.new_releases(country: 'JP')
 	end
 
 	def about
