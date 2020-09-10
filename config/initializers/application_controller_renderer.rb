d614f0dda8c4cfce499994bd011b65417d265181
vagrant <vagrant@localhost.localdomain>
2020-09-10 13:10:39 +0000

    add_user_show_and_edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a10b5657eb2a8af3beb11cb1dc954a10326d7b17e5dfd14b00d53fbaeac84dcad7ad9ff9772d4f13b609a376998f0f6b7e5e50120b835c2713636d7224201520'
