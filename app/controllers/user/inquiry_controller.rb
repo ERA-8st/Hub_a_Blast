2bc9afa3f2ef9ac95f6a70c3e13af428abf3027b
vagrant <vagrant@localhost.localdomain>
2020-09-19 13:32:21 +0000

    fixed_inquiry

--- app/controllers/user/inquiry_controller.rb
+++ app/controllers/user/inquiry_controller.rb
@@ -17,6 +17,7 @@ class User::InquiryController < ApplicationController
   def thanks
     @inquiry = Inquiry.new(params[:inquiry].permit(:name, :email, :message))    
     InquiryMailer.received_email(@inquiry).deliver
+    InquiryMailer.confirm_email(@inquiry).deliver
     render :action => 'thanks'
   end
   
