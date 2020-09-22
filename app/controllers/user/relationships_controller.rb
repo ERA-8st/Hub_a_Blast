3cfc3ef9138f8f54f9500dc1ce5be5fc4b7a08bc
vagrant <vagrant@localhost.localdomain>
2020-09-22 05:48:43 +0000

    add_Ajax

--- app/controllers/user/relationships_controller.rb
+++ app/controllers/user/relationships_controller.rb
@@ -6,7 +6,6 @@ class User::RelationshipsController < ApplicationController
 		following = current_user.follow(@user)
 		if following.save
 			flash[:success] = 'ユーザーをフォローしました'
-			redirect_back(fallback_location: root_path)
 		else
 			flash.now[:alert] = 'ユーザーのフォローに失敗しました'
 			redirect_back(fallback_location: root_path)
@@ -16,8 +15,7 @@ class User::RelationshipsController < ApplicationController
   def destroy
     following = current_user.unfollow(@user)
     if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_back(fallback_location: root_path)
+			flash[:success] = 'ユーザーのフォローを解除しました'
     else
       flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
       redirect_back(fallback_location: root_path)
