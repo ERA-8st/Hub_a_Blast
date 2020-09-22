519cd8cf6f14f5887586454a44da51c830b75c5b
vagrant <vagrant@localhost.localdomain>
2020-09-22 05:32:15 +0000

    add_follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,27 @@ class User < ApplicationRecord
   has_many :song_ratings, dependent: :destroy
   has_many :song_favorites, dependent: :destroy
 
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
+  # フォローしようとしているUserが自分では無いか
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  # フォローしている場合のみアンフォロー
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  # すでにフォローしていないか
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
 end
