bf97e34e907b34b6fd826c409331c26522609152
vagrant <vagrant@localhost.localdomain>
2020-09-24 04:42:30 +0000

    add_Ajax

--- app/controllers/user/messages_controller.rb
+++ app/controllers/user/messages_controller.rb
@@ -5,10 +5,12 @@ class User::MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
+      room = Room.find(params[:message][:room_id])
+      @messages = room.messages
     else
       flash[:alert] = "メッセージ送信に失敗しました。"
 		end
-		redirect_to  user_room_path(@message.room_id)
+		# redirect_to  user_room_path(@message.room_id)
 	end
 	
 end
