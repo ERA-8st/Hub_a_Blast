7b54b9606de7fe4207f4c7a2c689647e65f89654
vagrant <vagrant@localhost.localdomain>
2020-10-03 10:43:34 +0000

    omniauth-twitter

---
+++ db/migrate/20201003045858_add_columns_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+
+    add_index :users, [:provider, :uid], unique: true
+  end
+end
