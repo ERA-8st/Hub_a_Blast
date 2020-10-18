8c96e011d4126676d1179d5bd46824e6edd539cf
vagrant <vagrant@localhost.localdomain>
2020-10-18 08:53:11 +0000

    notification_header-template

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -9,25 +9,7 @@ class User::UsersController < ApplicationController
     # お気に入りを新しい順に並び替えて取得
     @song_favorites = @user.song_favorites.order("id DESC").limit(5)
     # DM
-    if user_signed_in?
-      @currentUserEntry=Entry.where(user_id: current_user.id)
-      @userEntry=Entry.where(user_id: @user.id)
-      unless @user.id == current_user.id
-        @currentUserEntry.each do |cu|
-          @userEntry.each do |u|
-            if cu.room_id == u.room_id then
-              @isRoom = true
-              @roomId = cu.room_id
-            end
-          end
-        end
-        if @isRoom
-        else
-          @room = Room.new
-          @entry = Entry.new
-        end
-      end
-    end
+    room_present?(@user)
     if @user == current_user
       # ユーザーのメッセージを新しい順に並び替えて同一のroomに対してのメッセージを除外
       @messages = @user.messages.order("id DESC").select(:room_id).distinct.limit(3)
