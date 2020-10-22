843e548c694bfd489dac80bd0e23ae24ff29b801
vagrant <vagrant@localhost.localdomain>
2020-10-22 10:14:57 +0000

    add_song_impression_count

--- app/controllers/user/song_impressions_controller.rb
+++ app/controllers/user/song_impressions_controller.rb
@@ -1,6 +1,18 @@
 class User::SongImpressionsController < ApplicationController
 
   def index
-    @top_impressions = SongImpression.group(:song_id).count(:song_id).to_a.sort {|a,b| a[1] <=> b[1]}.reverse
+    case params[:times]
+      when nil, "指定無し"
+        @top_impressions = SongImpression.group(:song_id).count(:song_id).to_a.sort {|a,b| a[1] <=> b[1]}.reverse
+      when "今日"
+        @top_impressions = SongImpression.where(created_at: Time.zone.now.all_day).group(:song_id).count(:song_id).to_a.sort {|a,b| a[1] <=> b[1]}.reverse
+        @time = "Today"
+      when "１週間"
+        @top_impressions = SongImpression.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day).group(:song_id).count(:song_id).to_a.sort {|a,b| a[1] <=> b[1]}.reverse
+        @time = "Week"
+      when "一ヶ月"
+        @top_impressions = SongImpression.where(created_at: 1.month.ago.beginning_of_day..Time.zone.now.end_of_day).group(:song_id).count(:song_id).to_a.sort {|a,b| a[1] <=> b[1]}.reverse
+        @time = "Month"
+    end
   end
 end
