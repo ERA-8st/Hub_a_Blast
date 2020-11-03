bb9e985cca5d77355f8bcdb7612b3be7f929dbba
vagrant <vagrant@localhost.localdomain>
2020-11-03 12:14:21 +0000

    add_notice_count

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -5,7 +5,8 @@ class MessageBroadcastJob < ApplicationJob
     user = User.find(message.user_id)
     @user_img = "#{user.profile_image.backend.directory}/#{user.profile_image_id}"
     @created_at = message.created_at.strftime("%Y-%m-%d %H:%M")
-    ActionCable.server.broadcast 'room_channel' , message: message ,created_at: @created_at, user_img: @user_img
+    @pair_user_id = Entry.where(room_id: message.room_id).where.not(user_id: message.user_id).first.user_id
+    ActionCable.server.broadcast 'room_channel' , message: message ,created_at: @created_at, user_img: @user_img, pair_user_id: @pair_user_id
   end
 
 end
