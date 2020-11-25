1820c3d1246c27005d564af1d1f0aae30bdae26b
vagrant <vagrant@localhost.localdomain>
2020-11-25 09:33:07 +0000

    add_feature_spec

--- spec/factories/song_comments.rb
+++ spec/factories/song_comments.rb
@@ -2,6 +2,10 @@ FactoryBot.define do
   factory :song_comment do
     association :user
     comment { "test" }
-    song_id { "2PnMMlB7UMaqQBYa1vGPHz" }
+    song_id { "4VXIryQMWpIdGgYR4TrjT1" }
+  end
+  factory :song_comment2, class: SongComment do
+    comment { "test" }
+    song_id { "4VXIryQMWpIdGgYR4TrjT1" }
   end
 end
