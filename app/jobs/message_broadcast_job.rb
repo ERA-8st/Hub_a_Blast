40c6cb22b33c1ff896afca0cc2f14c378e677b7d
era <hera123230@gmail.com>
2020-12-12 19:32:45 +0900

    fix_bug

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -3,7 +3,6 @@ class MessageBroadcastJob < ApplicationJob
 
   def perform(message)
     user = User.find(message.user_id)
-    @user_img = "#{user.profile_image.backend.directory}/#{user.profile_image_id}"
     @created_at = message.created_at.strftime("%Y-%m-%d %H:%M")
     @pair_user_id = Entry.where(room_id: message.room_id).where.not(user_id: message.user_id).first.user_id
     ActionCable.server.broadcast 'room_channel' , message: message ,created_at: @created_at, user_img: @user_img, pair_user_id: @pair_user_id
