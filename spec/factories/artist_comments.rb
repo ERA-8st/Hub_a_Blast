78795e1485bf5141e9005f23b973408e4ad04dea
era <hera123230@gmail.com>
2020-12-29 21:45:31 +0900

    modify_artist_comment

--- spec/factories/artist_comments.rb
+++ spec/factories/artist_comments.rb
@@ -8,8 +8,4 @@ FactoryBot.define do
     comment { "artist_comment2_test" }
     artist_id { "4STHEaNw4mPZ2tzheohgXB" }
   end
-  factory :artist_comment2, class: ArtistComment do
-    comment { "artist_comment2_test" }
-    artist_id {""}
-  end
 end
