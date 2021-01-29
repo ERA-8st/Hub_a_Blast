5b808c026783454017ecb43110925d3c10f05eea
era <hera123230@gmail.com>
2021-01-29 14:03:17 +0900

    remove_notification_method

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -6,14 +6,4 @@ class Entry < ApplicationRecord
   validates :user_id, :room_id, presence: true
   validates :room_id, uniqueness: { scope: :user_id  }
 
-  def create_notification_message!(current_user, message)
-    notification = current_user.active_notifications.new(
-      visited_id: user_id,
-      message: message.message,
-      action: 'message',
-      room_id: message.room_id
-    )
-    notification.save if notification.valid?
-  end
-
 end
