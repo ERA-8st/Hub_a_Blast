ead52f7ff52bfb2b3f757456f5ea58962bdab0ae
era <hera123230@gmail.com>
2021-02-25 17:42:49 +0900

    add_error_variables

--- app/controllers/user/album_comments_controller.rb
+++ app/controllers/user/album_comments_controller.rb
@@ -10,15 +10,7 @@ class User::AlbumCommentsController < ApplicationController
     if @album_comment.save 
       redirect_to user_spotify_album_show_path(@album_comment.album_id, page: @page)
     else
-      @album = RSpotify::Album.find(@album_comment.album_id)
-      @songs = @album.tracks
-      @album_comments = AlbumComment.where(album_id: @album.id).order("id DESC").page(params[:page]).per(5)
-      if user_signed_in?
-        @album_rating = current_user.album_ratings.find_by(album_id: @album.id)
-        @new_album_rating = current_user.album_ratings.new
-      end
-      @error = true
-      render "user/spotify/album_show"
+      set_error_variables(@album_comment.album_id, params[:page])
     end
   end
 
@@ -26,16 +18,8 @@ class User::AlbumCommentsController < ApplicationController
     if @comment.update(album_comment_params)
       redirect_to user_spotify_album_show_path(@comment.album_id, page: @page)
     else
-      @album = RSpotify::Album.find(@comment.album_id)
-      @songs = @album.tracks
       @album_comment = AlbumComment.new
-      @album_comments = AlbumComment.where(album_id: @album.id).order("id DESC").page(params[:page]).per(5)
-      if user_signed_in?
-        @album_rating = current_user.album_ratings.find_by(album_id: @album.id)
-        @new_album_rating = current_user.album_ratings.new
-      end
-      @error = true
-      render "user/spotify/album_show"
+      set_error_variables(@comment.album_id, params[:page])
     end
   end
 
@@ -44,10 +28,23 @@ class User::AlbumCommentsController < ApplicationController
     redirect_to user_spotify_album_show_path(@comment.album_id)
   end
 
+  def set_error_variables(album_id, page_params)
+    @album = RSpotify::Album.find(album_id)
+    @songs = @album.tracks
+    @album_comments = AlbumComment.where(album_id: @album.id).order("id DESC").page(page_params).per(5)
+    if user_signed_in?
+      @album_rating = current_user.album_ratings.find_by(album_id: @album.id)
+      @new_album_rating = current_user.album_ratings.new
+    end
+    @error = true
+    render "user/spotify/album_show"
+  end
+
   private
 
   def album_comment_params
     params.require(:album_comment).permit(:comment)
   end
+  
 
 end
