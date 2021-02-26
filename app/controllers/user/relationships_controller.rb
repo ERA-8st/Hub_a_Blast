62de2b5b102b929a386e607a38ab880b37388a8d
era <hera123230@gmail.com>
2021-02-26 16:00:08 +0900

    add_set_follows

--- app/controllers/user/relationships_controller.rb
+++ app/controllers/user/relationships_controller.rb
@@ -1,14 +1,10 @@
 class User::RelationshipsController < ApplicationController
 
-  before_action :authenticate_user!
-  before_action :set_user
+  prepend_before_action :authenticate_user!
+  before_action :set_user, :set_follows
 
   def create
     following = current_user.follow(@user)
-    if params[:relationship][:index_user_id]
-      @index_user = User.find(params[:relationship][:index_user_id])
-      @follows = @index_user.relationships
-    end
     if following.save
       flash[:success] = 'ユーザーをフォローしました'
       @user.create_notification_follow!(current_user)
@@ -20,10 +16,6 @@ class User::RelationshipsController < ApplicationController
 
   def destroy
     following = current_user.unfollow(@user)
-    if params[:relationship][:index_user_id]
-      @index_user = User.find(params[:relationship][:index_user_id])
-      @follows = @index_user.relationships
-    end
     if following.destroy
       flash[:success] = 'ユーザーのフォローを解除しました'
     else
@@ -36,6 +28,12 @@ class User::RelationshipsController < ApplicationController
   
   def set_user
     @user = User.find(params[:relationship][:follow_id])
+  end
+
+  def set_follows
+    if params[:relationship][:index_user_id]
+      @follows = User.find(params[:relationship][:index_user_id]).relationships
+    end
   end
 
 end
